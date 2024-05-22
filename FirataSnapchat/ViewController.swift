824f9fad6d379f355537250967ef3e043fbba61a
JorgeManu <jorge.firata@tecsup.edu.pe>
2024-05-22 11:17:01 -0500

    Initial Commit

---
+++ FirataSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirataSnapchat
+//
+//  Created by Jorge Firata Quiuspe Jorge Manuel on 22/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
